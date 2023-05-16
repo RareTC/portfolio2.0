25e469c28d91787f09deda887ca808e191c50948
RareTC <trevorjcampbell@aol.com>
2023-05-16 12:57:17 -0700

    nav toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 
@@ -6,6 +7,9 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+
+      </Routes>
       <Footer />
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
 import './Navbar.css';
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import ReorderIcon from '@mui/icons-material/Reorder';
+import Logo from '../../assets/tclogo.png';
+
 
 export default function Navbar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+
+  const location = useLocation();
+  useEffect(() => {
+    setExpandNavbar(false)
+  }, [location])
+
   return (
-    <div className='navbar'>
-        Navbar
+    <div className='navbar' id={expandNavbar ? "open" : "close"}>
+      <div className="toggleButton">
+        <button onClick={() => { setExpandNavbar((prev) => !prev) }}>
+          <ReorderIcon />
+        </button>
+      </div>
+      <div className="links">
+        <img className='logo' src={Logo} alt="" />
+        <Link to="/"> Home </Link>
+        <Link to="/projects"> Projects </Link>
+        {/* <Link to="/experience"> Experience </Link> */}
+        <Link to="/resume"> Resume </Link>
+        <Link to="/contact"> Contact </Link>
+        <Link to="/about"> About </Link>
+      </div>
     </div>
   )
 }
