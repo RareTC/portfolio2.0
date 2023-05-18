afe434d1cbeae8327bd9443bb7c797f597ca8e8f
RareTC <trevorjcampbell@aol.com>
2023-05-18 10:17:03 -0700

    settimeout for email sent

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import emailjs from '@emailjs/browser';
 import './Contact.css';
 import SendIcon from '@mui/icons-material/Send';
@@ -6,11 +6,19 @@ import SendIcon from '@mui/icons-material/Send';
 
 export default function Contact({colors}) {
 
-  
   const [emailSent, setEmailSent] = useState(false);
   const form = useRef();
   const [c0, c1, c2, c3, c4] = colors;
   
+  useEffect(() => {
+    if (emailSent) {
+      const timer = setTimeout(() => {
+        setEmailSent(false);
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [emailSent]);
+
     const sendEmail = (e) => {
       e.preventDefault();
   
@@ -24,27 +32,27 @@ export default function Contact({colors}) {
         });
     };
 
-    return (
-        <>
-          <div className='contactForm' id='contact' style={{backgroundColor: c3}}>
-            <div className='formcontainer' style={{backgroundColor: c2}}>
-            {!emailSent && (
-              <>
-                <h1 style={{color: c1}}>Say Hello!</h1>
-                <h3 style={{color: c1}}>I look forward to hearing from you!</h3>
-                <div className='emailform'>
-                  <form ref={form} onSubmit={sendEmail}>
-                    {/* <label>Name</label> */}
-                    <div className='inputform'>
-                      <input type="text" name="form_name" placeholder="Name" required 
-                      style={{border: `2px solid ${c1}`, caretColor: c0, color: c0,}}/>
-                      <input type="email" name="form_email" placeholder='Email' required 
-                      style={{border: `2px solid ${c1}`, caretColor: c0, color: c0,}}/>
-                    </div>
-                    <textarea name="message" placeholder='Enter Message' required
-                     style={{border: `2px solid ${c1}`, caretColor: c0, color: c0,}}/>
-                    <button id='contactButton' type="submit"
-                    style={{backgroundColor: c4, color: c1}}
+
+  return (
+    <>
+      <div className='contactForm' id='contact' style={{ backgroundColor: c3 }}>
+        <div className='formcontainer' style={{ backgroundColor: c2 }}>
+          {!emailSent && (
+            <>
+              <h1 style={{ color: c1 }}>Say Hello!</h1>
+              <h3 style={{ color: c1 }}>I look forward to hearing from you!</h3>
+              <div className='emailform'>
+                <form ref={form} onSubmit={sendEmail}>
+                  <div className='inputform'>
+                    <input type="text" name="form_name" placeholder="Name" required
+                      style={{ border: `2px solid ${c1}`, caretColor: c0, color: c0, }} />
+                    <input type="email" name="form_email" placeholder='Email' required
+                      style={{ border: `2px solid ${c1}`, caretColor: c0, color: c0, }} />
+                  </div>
+                  <textarea name="message" placeholder='Enter Message' required
+                    style={{ border: `2px solid ${c1}`, caretColor: c0, color: c0, }} />
+                  <button id='contactButton' type="submit"
+                    style={{ backgroundColor: c4, color: c1 }}
                     onMouseEnter={(e) => {
                       e.target.style.backgroundColor = c1;
                       e.target.style.color = c4;
@@ -53,19 +61,19 @@ export default function Contact({colors}) {
                       e.target.style.backgroundColor = c4;
                       e.target.style.color = c1;
                     }}>
-                      Send <SendIcon />
-                    </button>
+                    Send <SendIcon />
+                  </button>
                 </form>
-                </div>
-              </>
-            )}
-            {emailSent && 
-            <div className='formSent' style={{color: c1}}>
+              </div>
+            </>
+          )}
+          {emailSent &&
+            <div className='formSent' style={{ color: c1 }}>
               Thank you! Your Message Is On The Way!
             </div>}
-          </div>
-              </div>
+        </div>
+      </div>
 
-        </>
-      );
+    </>
+  );
 }
