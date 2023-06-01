c36d2af5d4b546c32d5cde410608656aaa7d0a07
RareTC <trevorjcampbell@aol.com>
2023-05-31 17:30:05 -0700

    fixing mobile not rendering.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -42,11 +42,11 @@ export default function Header({ selectedColor, onColorChange }) {
         <span style={{ "--i": 9 }}></span>
         <span style={{ "--i": 7 }}></span>
         <span style={{ "--i": 15 }}></span>
-        {/* <span style={{ "--i": 12 }}></span>
+        <span style={{ "--i": 12 }}></span>
         <span style={{ "--i": 24 }}></span>
         <span style={{ "--i": 10 }}></span>
         <span style={{ "--i": 14 }}></span>
-        <span style={{ "--i": 23 }}></span> */}
+        <span style={{ "--i": 23 }}></span>
         <span style={{ "--i": 18 }}></span>
         <span style={{ "--i": 19 }}></span>
         <span style={{ "--i": 16 }}></span>
