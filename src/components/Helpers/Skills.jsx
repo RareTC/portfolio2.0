ba144894ce020f0aab1b79e537741a00030ee95e
RareTC <trevorjcampbell@aol.com>
2023-06-11 14:52:31 -0700

    JS Icon Change

--- src/components/Helpers/Skills.jsx
+++ src/components/Helpers/Skills.jsx
@@ -1,7 +1,7 @@
 import { FaHtml5, FaCss3, FaPython, FaBootstrap, FaNodeJs, FaReact } from 'react-icons/fa';
-import { DiJavascript1, DiMongodb } from 'react-icons/di';
+import { DiMongodb } from 'react-icons/di';
 // import { GrReactjs } from 'react-icons/gr';
-import { SiPostgresql, SiExpress, SiHeroku } from 'react-icons/si';
+import { SiPostgresql, SiExpress, SiHeroku, SiJavascript } from 'react-icons/si';
 import { BsSlack, BsTrello } from 'react-icons/bs';
 import { TbBrandZoom } from 'react-icons/tb';
 
@@ -11,7 +11,7 @@ export const Skills = [
       languages: [
         { name: "HTML", icon: <FaHtml5 /> },
         { name: "CSS", icon: <FaCss3 /> },
-        { name: "JavaScript", icon: <DiJavascript1 /> },
+        { name: "JavaScript", icon: <SiJavascript /> },
         { name: "React.js", icon: <FaReact /> },
         { name: "Node.js", icon: <FaNodeJs /> },
         { name: "MongoDB", icon: <DiMongodb /> },
